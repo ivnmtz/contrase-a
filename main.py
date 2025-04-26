7f42dbf6997ca46b8582abc6b408960cd0f88215
ivnmtz <alcalaisaid@gmail.com>
2025-04-26 10:32:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-import random
-caracteres="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-longitud=int(input("introduzca la longitud"))
-contrasena=""
-for i in range(longitud):
-    contrasena += random.choice(caracteres)
-print(contrasena)
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run()
