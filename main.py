7057c169c5aab73cb35a5138de15575ae92211db
ivnmtz <alcalaisaid@gmail.com>
2025-05-10 08:42:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import discord
+import os
+import random
+import requests
 from discord.ext import commands
-from bot_logic import gen_pass
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='$', intents=intents)
-
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hola, soy un bot {bot.user}!')
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    # ¡Y así es como se puede sustituir el nombre del fichero desde una variable!
+    with open(f'images/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f'Bye :)')
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)  
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
-
-
-@bot.command()
-async def password(ctx):
-    await ctx.send(gen_pass(10))
-
-
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-bot.run()
+async def chiste(ctx):
+    chistes = [
+        "¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter.",
+        "¿Qué le dice un gusano a otro gusano? Voy a dar una vuelta a la manzana.",
+        "¿Por qué el libro de matemáticas está triste? Porque tiene demasiados problemas."
+    ]
+    await ctx.send(random.choice(chistes))
+    
